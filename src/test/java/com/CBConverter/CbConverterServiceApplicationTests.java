e717a3c11f2339b1872583f71e99505defeabf0d
vkalko <vkalko@tsconsulting.com>
2020-08-06 10:17:44 +0300

    fit: добавлен тест на проверку работы API ЦБ

--- src/test/java/com/CBConverter/CbConverterServiceApplicationTests.java
+++ src/test/java/com/CBConverter/CbConverterServiceApplicationTests.java
@@ -1,13 +1,27 @@
 package com.CBConverter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static java.lang.String.format;
+import static org.springframework.util.Assert.isTrue;
+import static org.springframework.util.Assert.notNull;
+
+@Slf4j
 @SpringBootTest
 class CbConverterServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void checkConnection() throws Exception {
+        String url = "http://www.cbr.ru/scripts/XML_daily.asp";
+        HttpURLConnection connection;
+        connection = (HttpURLConnection) new URL(url).openConnection();
+        notNull(connection, "Соединение не установлено. Проверьте интернет соединение");
+        isTrue(connection.getResponseCode() == HttpStatus.OK.value(), format("StatusCode: %s", connection.getResponseCode()));
+    }
 }
